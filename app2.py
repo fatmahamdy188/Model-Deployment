9c3cec45d81615f9de7d750130ffb2151a75abe5
fatmahamdy188 <143092084+fatmahamdy188@users.noreply.github.com>
2024-05-04 12:42:53 +0000

    Local and live modifaction

--- app2.py
+++ app2.py
@@ -116,4 +116,4 @@ def handle_generic_error(error):
     return jsonify(result), 500
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", debug=True)
